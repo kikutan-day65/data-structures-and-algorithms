33f82e2cebeec6d0c9863a888431f558d3e889e9
Haruto Mori <baystarsman@outlook.jp>
2023-02-14 12:12:13 +0100

    build a fuinction to print out the calues in linked list

--- linked_lists/singly-LL.py
+++ linked_lists/singly-LL.py
@@ -9,10 +9,17 @@ class LinkedList:
 
     # constructor
     def __init__(self, value):
-        new_node = Node(value) # create a first node
-        self.head = new_node # assign the first one as a head
-        self.tail = new_node # assign the first one as a tail
-        self.length = 1 # keep track of the length
+        new_node = Node(value)  # create a first node
+        self.head = new_node    # assign the first one as a head
+        self.tail = new_node    # assign the first one as a tail
+        self.length = 1     # keep track of the length
+    
+    # print linked list
+    def print_list(self):
+        temp = self.head    # assign the first node to temp
+        while temp is not None: # loop until temp reaches None
+            print(temp.value)   # print
+            temp = temp.next    # assign temp to the next value
 
 my_ll = LinkedList(4)
 print(my_ll.head.value)
