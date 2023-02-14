d6860d7a4955c6d822e639cab41c54d7c05b3c53
Haruto Mori <baystarsman@outlook.jp>
2023-02-14 12:02:55 +0100

    build a linked list  constructor

--- linked_lists/singly-LL.py
+++ linked_lists/singly-LL.py
@@ -3,3 +3,16 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+# LinkedList class
+class LinkedList:
+
+    # constructor
+    def __init__(self, value):
+        new_node = Node(value) # create a first node
+        self.head = new_node # assign the first one as a head
+        self.tail = new_node # assign the first one as a tail
+        self.length = 1 # keep track of the length
+
+my_ll = LinkedList(4)
+print(my_ll.head.value)
