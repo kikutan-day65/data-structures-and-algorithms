4a6fdfd6d6ecb75da1a774cc971a2e44a17edbae
Haruto Mori <baystarsman@outlook.jp>
2023-02-15 12:25:40 +0100

    build a funciton to push the item into the stack

--- stacks/stacks.py
+++ stacks/stacks.py
@@ -7,7 +7,7 @@ class Stack:
     def __init__(self, value):
         new_node = Node(value)   # initialize the node
         self.top = new_node # set the top of stack
-        self.heght = 1  # keep track of length of stack
+        self.height = 1  # keep track of length of stack
 
     #=====================================================================================================
 
@@ -19,6 +19,22 @@ class Stack:
 
     #=====================================================================================================
 
+    def push(self, value):
+        new_node = Node(value)
+
+        # when the stack is empty
+        if self.height == 0:
+            self.top = new_node
+            self.height += 1
+        
+        # otherwise
+        else:
+            new_node.next = self.top
+            self.top = new_node
+
+        self.height += 1
+    
+    #=====================================================================================================
 
 
 
@@ -26,3 +42,9 @@ my_stack = Stack(4)
 my_stack.print_stack()
 
 print()
+
+push_stack = Stack(3)
+push_stack.push(12)
+push_stack.push(15)
+push_stack.push(2)
+push_stack.print_stack()
