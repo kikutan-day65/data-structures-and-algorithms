8bc79bca3c2d686b7e710ece433ecc388273d027
Haruto Mori <baystarsman@outlook.jp>
2023-02-16 12:03:17 +0100

    build a dequeue function and fix the enqueue function

--- queues/queues.py
+++ queues/queues.py
@@ -26,15 +26,33 @@ class Queue:
         if self.first is None:
             self.first = new_node
             self.last = new_node
-            self. length += 1
 
         else:
             self.last.next = new_node
-            new_node = self.last
-            self.length += 1
+            self.last = new_node
+        self.length += 1
+        return True
 
     #=====================================================================================================
 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.first
+        
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        
+        else:
+            self.first = self.first.next
+            temp.next = None
+
+        self.length -= 1
+
+        return temp
+    
 
 my_queue = Queue(5)
 my_queue.print_queue()
@@ -45,3 +63,13 @@ en_queue = Queue(3)
 en_queue.enqueue(4)
 en_queue.enqueue(5)
 en_queue.print_queue()
+
+print()
+
+de_queue = Queue(7)
+de_queue.enqueue(8)
+de_queue.enqueue(9)
+de_queue.enqueue(10)
+de_queue.enqueue(11)
+de_queue.dequeue()
+de_queue.print_queue()
