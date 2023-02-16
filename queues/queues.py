9a4bdd9b85bba8cf2ca41b7082faab588e392fe7
Haruto Mori <baystarsman@outlook.jp>
2023-02-16 11:48:27 +0100

    build a function to enqueue the item

--- queues/queues.py
+++ queues/queues.py
@@ -15,11 +15,33 @@ class Queue:
     def print_queue(self):
         temp = self.first
         while temp is not None:
-            print(temp.value)
+            print(temp.value, end=" ")
             temp = temp.next
 
     #=====================================================================================================
     
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+            self. length += 1
+
+        else:
+            self.last.next = new_node
+            new_node = self.last
+            self.length += 1
+
+    #=====================================================================================================
+
 
 my_queue = Queue(5)
 my_queue.print_queue()
+
+print()
+
+en_queue = Queue(3)
+en_queue.enqueue(4)
+en_queue.enqueue(5)
+en_queue.print_queue()
