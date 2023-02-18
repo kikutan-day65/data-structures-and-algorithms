0676758d1d55221587de6aa1fc9ade51772587a7
Haruto Mori <baystarsman@outlook.jp>
2023-02-18 13:19:48 +0100

    build a constructor and print function for doubly linked list

--- linked_lists/doubly-LL.py
+++ linked_lists/doubly-LL.py
@@ -1,6 +1,30 @@
+# Node class
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
 
+# DoublyLinkedList class
+class DoublyLinkedList:
+
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    #=====================================================================================================
+
+    def print_list(self):
+        temp = self.head
+        
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    #=====================================================================================================
+    
+
+doubly_LL = DoublyLinkedList(7)
+doubly_LL.print_list()
