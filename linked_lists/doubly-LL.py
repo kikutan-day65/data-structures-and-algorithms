e59f2f9677c4afc3a0c74c08371729e852024092
Haruto Mori <baystarsman@outlook.jp>
2023-02-18 13:31:10 +0100

    build a append function for doubly linked list

--- linked_lists/doubly-LL.py
+++ linked_lists/doubly-LL.py
@@ -20,11 +20,40 @@ class DoublyLinkedList:
         temp = self.head
         
         while temp is not None:
-            print(temp.value)
+            print(temp.value, end=" ")
             temp = temp.next
 
     #=====================================================================================================
     
+    def append(self, value):
+        new_node = Node(value)
+
+        # when the doubly linked list is empty
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        # otherwise
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        
+        self.length += 1
+
+        return True
+
+    #=====================================================================================================
+
 
 doubly_LL = DoublyLinkedList(7)
 doubly_LL.print_list()
+
+print()
+
+append_doubly_LL = DoublyLinkedList(2)
+append_doubly_LL.append(3)
+append_doubly_LL.append(4)
+append_doubly_LL.print_list()
+
+print()
