3b91b17ad2b4f1770ec783222e4b7e1ee7e138f6
Haruto Mori <baystarsman@outlook.jp>
2023-02-19 11:53:39 +0100

    buiuld a constoructor for bin search tree

--- trees/bin_search_tree.py
+++ trees/bin_search_tree.py
@@ -3,3 +3,16 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None    # top-most node of the bin search tree
+
+    #=====================================================================================================
+
+
+
+my_tree = BinarySearchTree()
+print(my_tree.root)
+
+print()
