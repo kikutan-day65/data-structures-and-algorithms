3c56593ff9dfb2b63c99bb0f1b2a8e9eabb8a00a
Haruto Mori <baystarsman@outlook.jp>
2023-02-20 15:11:54 +0100

    build a function to remove the edge

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -25,16 +25,45 @@ class Graph:
             return True
         return False
     
+    #=====================================================================================================
 
-vertex_graph = Graph()
-vertex_graph.add_vertex('A')
-vertex_graph.add_vertex('B')
-vertex_graph.print_graph()
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    #=====================================================================================================
+
+add_vertex_graph = Graph()
+add_vertex_graph.add_vertex('A')
+add_vertex_graph.add_vertex('B')
+add_vertex_graph.print_graph()
 
 print()
 
-edge_graph = Graph()
-edge_graph.add_vertex(1)
-edge_graph.add_vertex(2)
-edge_graph.add_edge('D', 'F')
-edge_graph.print_graph()
+add_edge_graph = Graph()
+add_edge_graph.add_vertex('D')
+add_edge_graph.add_vertex('F')
+add_edge_graph.add_edge('D', 'F')
+add_edge_graph.print_graph()
+
+print()
+
+rm_edge_graph = Graph()
+rm_edge_graph.add_vertex('A')
+rm_edge_graph.add_vertex('B')
+rm_edge_graph.add_vertex('C')
+rm_edge_graph.add_vertex('D')
+
+rm_edge_graph.add_edge('A', 'B')
+rm_edge_graph.add_edge('B', 'C')
+rm_edge_graph.add_edge('C', 'A')
+
+rm_edge_graph.remove_edge('A', 'D')
+
+rm_edge_graph.print_graph()
