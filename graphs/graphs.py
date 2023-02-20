71dbe07f8afc474bf18ce64d982219cf5c145078
Haruto Mori <baystarsman@outlook.jp>
2023-02-20 14:43:33 +0100

    build a function to print the graph and to add the vertex

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,3 +1,28 @@
 class Graph:
     def __init__(self):
         self.adj_list = {}
+
+    #=====================================================================================================
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+
+    #=====================================================================================================
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    #=====================================================================================================
+
+
+add_graph = Graph()
+add_graph.add_vertex('A')
+add_graph.add_vertex('B')
+add_graph.print_graph()
+
+print()
+
