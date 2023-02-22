8983cfa8d8d047ad1acd18fad150e1773f00d508
Haruto Mori <baystarsman@outlook.jp>
2023-02-22 15:48:05 +0100

    build a function to check by recursive way if the bst contains a specific number

--- trees/bin_search_tree.py
+++ trees/bin_search_tree.py
@@ -79,6 +79,27 @@ class BinarySearchTree:
     """
 
 
+    #=====================================================================================================
+
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+        
+        if value == current_node.value:
+            return True
+        
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+    
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+
+    #=====================================================================================================
+
+
 
 my_tree = BinarySearchTree()
 print(my_tree.root)
@@ -106,3 +127,20 @@ contain_tree.insert(77)
 contain_tree.insert(32)
 print(contain_tree.contains(67))
 print(contain_tree.contains(9))
+
+print()
+
+r_bst = BinarySearchTree()
+r_bst.insert(47)
+r_bst.insert(21)
+r_bst.insert(76)
+r_bst.insert(18)
+r_bst.insert(27)
+r_bst.insert(53)
+r_bst.insert(82)
+
+print('BST Contains: 27: ')
+print(r_bst.r_contains(27))
+
+print('BST Contains: 17: ')
+print(r_bst.r_contains(17))
